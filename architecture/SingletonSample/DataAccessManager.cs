7dac47d3cabf4cca15948c9ff34417c8cac623d7
pneefe <patrik.neefe@gmx.de>
2019-05-13 15:37:38 +0200

    Notizen + Singleton

---
+++ architecture/SingletonSample/DataAccessManager.cs
@@ -0,0 +1,25 @@
+﻿namespace SingletonSample
+{
+    class DataAccessManager
+    {
+        private static DataAccessManager _instance;
+        private DataAccessManager()
+        {
+
+        }
+
+        public DataAccessManager Create()
+        {
+            if (_instance == null)
+            {
+                _instance = new DataAccessManager();
+            }
+            return _instance;
+        }
+
+        public void Save()
+        {
+            //Durch alles iterieren udn Änderungen speichern
+        }
+    }
+}
